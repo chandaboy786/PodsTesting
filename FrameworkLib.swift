23102a31f748e4b40b3348bdcec1f56ae6e34049
Muhammad Farhan <muhammad.farhan@golootlo.pk>
2023-05-24 16:58:04 +0500

    commit changes

---
+++ FrameworkLib.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkLib.swift
+//  FrameworkLibs
+//
+//  Created by Muhammad Farhan on 24/05/2023.
+//
+
+public final class FrameworkLib {
+
+    let name = "FrameworkLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- FrameworkLibsTests/FrameworkLibsTests.swift
+++ FrameworkLibsTests/FrameworkLibsTests.swift
@@ -10,8 +10,11 @@ import XCTest
 
 final class FrameworkLibsTests: XCTestCase {
 
+    var swiftyLib: FrameworkLib!
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        swiftyLib = FrameworkLib()
     }
 
     override func tearDownWithError() throws {
@@ -24,8 +27,13 @@ final class FrameworkLibsTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        testAdd()
     }
 
+    func testAdd() {
+        XCTAssertEqual(swiftyLib.add(a: 1, b: 1), 2)
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
