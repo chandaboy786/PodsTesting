e19e8cac5c2cc3f88dc3d16f7cc7f8e39ba4147a
Muhammad Farhan <muhammad.farhan@golootlo.pk>
2023-05-24 16:51:01 +0500

    Initial Commit

---
+++ FrameworkLibs/FrameworkLibs.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkLibs.h
+//  FrameworkLibs
+//
+//  Created by Muhammad Farhan on 24/05/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkLibs.
+FOUNDATION_EXPORT double FrameworkLibsVersionNumber;
+
+//! Project version string for FrameworkLibs.
+FOUNDATION_EXPORT const unsigned char FrameworkLibsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkLibs/PublicHeader.h>
+
+
